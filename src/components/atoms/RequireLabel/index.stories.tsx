67467a4cd6203c2ec8344dafc731caa52e588262
Yuki Abe <12268087+yukia3@users.noreply.github.com>
2020-10-07 19:14:44 +0900

    Add RequireLabel

---
+++ src/components/atoms/RequireLabel/index.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { RequireLabel } from './index';
+
+export default {
+  title: 'Atoms/RequireLabel',
+};
+
+export const copyright = () => {
+  return <RequireLabel />;
+};
